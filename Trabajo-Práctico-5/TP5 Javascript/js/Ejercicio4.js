47beedb08f3076c185d2f7482360cff61923e1e4
Bruno Tarditi <brunotarditi@hotmail.com>
2020-10-07 19:59:51 -0300

    tp 5 programación 3

--- Trabajo-Práctico-5/TP5 Javascript/js/Ejercicio4.js
+++ Trabajo-Práctico-5/TP5 Javascript/js/Ejercicio4.js
@@ -1,15 +1,15 @@
 var sueldo = parseFloat(prompt("Indique su sueldo"));
 var antiguedad = parseInt(prompt("Indique su antigüedad"));
-let h = document.getElementById('miH');
+var h = document.getElementById('miH');
 
 if (sueldo < 500 && antiguedad >= 10) {
-    sueldo += sueldo * 0.20;
+    sueldo += (sueldo * 0.20);
     h.innerText = 'El sueldo es: ' + sueldo + ' con un aumento del 20%.';
-}
-if (sueldo < 500 && antiguedad < 10) {
-    sueldo += sueldo * 0.05;
+    
+}else if (sueldo < 500 && antiguedad < 10) {
+    sueldo += (sueldo * 0.05);
     h.innerText = 'El sueldo es: ' + sueldo + ' con un aumento del 5%.';
-}
-if (sueldo >= 500) {
+
+} else if (sueldo >= 500) {
     h.innerText = 'El sueldo es: ' + sueldo + ' sin descuento.';
 }
