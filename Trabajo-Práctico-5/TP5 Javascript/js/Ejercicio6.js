9347b79917b24c095a29098fd13e87895ca0c33e
Bruno Tarditi <brunotarditi@hotmail.com>
2020-10-10 21:20:02 -0300

    TP5 PROG 3 corrección punto 6

--- Trabajo-Práctico-5/TP5 Javascript/js/Ejercicio6.js
+++ Trabajo-Práctico-5/TP5 Javascript/js/Ejercicio6.js
@@ -1,13 +1,8 @@
-const button = document.getElementById('button');
-const modal = document.getElementById('modal');
-button.addEventListener('click', openModal, false);
-modal.addEventListener('click',closeModal, false);
-
-function openModal() {
-    modal.classList.add('modal_show');
-}
-
-function closeModal(){
-    modal.classList.remove('modal_show') 
-     
+function abrirVentanaModal() {
+    var w = 600;
+    var h = 300;
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2);
+    var ventana = open('','','status=yes,width='+w+',height='+h+',menubar=yes,top='+top+',left='+left+'');
+    ventana.document.write("<h3>Ventana de 600 de ancho x 300 de alto!</h3>");
 }
